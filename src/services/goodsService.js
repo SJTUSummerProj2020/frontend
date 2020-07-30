8d3442d5eaefb5fe51bb4fe8805eb4feace7ce45
shenhliu <55398344+shenhliu@users.noreply.github.com>
2020-07-30 10:40:46 +0800

    fix：增加get delete put方法传输参数

--- src/services/goodsService.js
+++ src/services/goodsService.js
@@ -1,67 +1,66 @@
-import {postRequest} from "../utils/ajax";
-import {useCallback} from "react";
+import {postRequest,getRequest,deleteRequest,putRequest} from "../utils/ajax";
 
-export const getAllGoods = (data, callback) => {
-    const url = `http://localhost:8080/getAllGoods`;
-    postRequest(url, data, callback);
+export const getAllGoods = (data,callback) => {
+    const url = `http://localhost:8080/goods/getAllGoods`;
+    getRequest(url, data, callback);
 };
 
-export const getGoodsByGoodsId = (data, callback) => {
-    const url = `http://localhost:8080/getGoodsByGoodsId`;
-    postRequest(url, data, callback);
+export const getGoodsByGoodsId = (data,callback) => {
+    const url = `http://localhost:8080/goods/getGoodsByGoodsId`;
+    getRequest(url, data, callback);
 };
 
-export const getGoodsByGoodsType = (data, callback) => {
-    const url = `http://localhost:8080/getGoodsByGoodsType`;
-    postRequest(url, data, callback);
+export const getGoodsByGoodsType = (data,callback) => {
+    const url = `http://localhost:8080/goods/getGoodsByGoodsType`;
+    getRequest(url, data, callback);
 };
 
-export const getGoodsByName = (data, callback) => {
-    const url = `http://localhost:8080/getGoodsByName`;
-    postRequest(url, data, callback);
+export const getGoodsByName = (data,callback) => {
+    const url = `http://localhost:8080/goods/getGoodsByName`;
+    getRequest(url, data, callback);
 };
 
-export const getPopularGoods = (data, callback) => {
-    const url = `http://localhost:8080/getPopularGoods`;
-    postRequest(url, data, callback);
+export const getPopularGoods = (data,callback) => {
+    const url = `http://localhost:8080/goods/getPopularGoods`;
+    getRequest(url, data, callback);
 };
 
-export const getAllAuctions = (data, callback) => {
-    const url = `http://localhost:8080/getAllAuctions`;
-    postRequest(url, data, callback);
-};
+export const getAllAuctions = (data,callback) =>{
+    const url = `http://localhost:8080/goods/getAllAuctions`;
+    getRequest(url,data,callback);
+}
 
-export const getAuctionByAuctionId = (data, callback) => {
-    const url = `http://localhost:8080/getAuctionByAuctionId`;
-    postRequest(url, data, callback);
-};
+export const getAuctionByAuctionId = (data,callback) =>{
+    const url = `http://localhost:8080/goods/getAuctionByAuctionId`;
+    getRequest(url,data,callback);
+}
 
-export const updateAuction = (data, callback) => {
-    const url = `http://localhost:8080/updateAuction`;
-    postRequest(url, data, callback);
-};
+export const updateAuction = (data,callback) =>{
+    const url = `http://localhost:8080/goods/updateAuction`;
+    postRequest(url,data,callback);
+}
 
-export const deleteGoodsByGoodsId = (data, callback) => {
-    const url = `http://localhost:8080/deleteGoodsByGoodsId`;
-    postRequest(url, data, callback);
-};
+export const deleteGoodsByGoodsId = (data,callback) =>{
+    const url = `http://localhost:8080/goods/deleteGoodsByGoodsId`;
+    deleteRequest(url,data,callback);
+}
 
-export const addAuction = (data, callback) => {
-    const url = `http://localhost:8080/addAuction`;
-    postRequest(url, data, callback);
-};
+export const addAuction = (data,callback) => {
+    const url = `http://localhost:8080/goods/addAuction`;
+    postRequest(url,data,callback);
+}
 
-export const editGoods = (data, callback) => {
-    const url = `http://localhost:8080/editGoods`;
-    postRequest(url, data, callback);
-};
+export const editGoods = (data,callback) => {
+    const url = `http://localhost:8080/goods/editGoods`;
+    postRequest(url,data,callback);
+}
 
-export const deleteAuctionByAuctionId = (data, callback) => {
-    const url = `http://localhost:8080/deleteAuctionByAuctionId`;
-    postRequest(url, data, callback);
-};
+export const deleteAuctionByAuctionId = (data,callback) => {
+    const url = `http://localhost:8080/goods/deleteAuctionByAuctionId`;
+    deleteRequest(url,data,callback);
+}
 
-export const editAuction = (data, callback) => {
-    const url = `http://localhost:8080/editAuction`;
-    postRequest(url, data, callback);
-};
+export const editAuction = (data,callback) => {
+    const url = `http://localhost:8080/goods/editAuction`;
+    postRequest(url,data,callback);
+}
