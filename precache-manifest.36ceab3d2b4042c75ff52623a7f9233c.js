e06be6efbf302c11c0930ac0e71c93997b3e8394
HaoningLan <48608756+HaoningLan@users.noreply.github.com>
2020-09-08 07:53:14 +0000

    Deploying to gh-pages from  @ 27c8021c2fae2e57e4ef64f860c17be758e203d4 🚀

---
+++ precache-manifest.36ceab3d2b4042c75ff52623a7f9233c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54061081816478dde7cf5bd0847a08b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "455b5d4cbd892e2e319d",
+    "url": "/static/css/2.fd0a8594.chunk.css"
+  },
+  {
+    "revision": "a35246bd10121aef5ab7",
+    "url": "/static/css/main.142b2343.chunk.css"
+  },
+  {
+    "revision": "455b5d4cbd892e2e319d",
+    "url": "/static/js/2.2a74d270.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.2a74d270.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a35246bd10121aef5ab7",
+    "url": "/static/js/main.fe34839d.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "7d7eabe7d61e7bc615d27e7e856197bc",
+    "url": "/static/media/1.7d7eabe7.jpg"
+  },
+  {
+    "revision": "be6e56c2490dc6770bc43f83e96dd9f0",
+    "url": "/static/media/2.be6e56c2.jpg"
+  },
+  {
+    "revision": "46439757e74c14c644278c458603d454",
+    "url": "/static/media/3.46439757.jpg"
+  },
+  {
+    "revision": "0902a3a2b361276237e4ed132d55154e",
+    "url": "/static/media/bg2.0902a3a2.png"
+  }
+]);
