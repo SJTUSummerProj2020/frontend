ec4241b6fbdd696f048fa6fed0074770e5f94312
CKKKKKKKKK <41709469+CKKKKKKKKK@users.noreply.github.com>
2020-09-08 15:02:01 +0800

    Update services

--- src/services/goodsService.js
+++ src/services/goodsService.js
@@ -1,67 +1,68 @@
 import { postRequest, getRequest, deleteRequest, putRequest } from '../utils/ajax';
 
+const header = 'http://ec2-100-25-4-241.compute-1.amazonaws.com:8080/';
 export const getAllGoods = (data, callback) => {
-  const url = 'http://localhost:8080/goods/getAllGoods';
+  const url = header + 'goods/getAllGoods';
   getRequest(url, data, callback);
 };
 
 export const getGoodsByGoodsId = (data, callback) => {
-  const url = 'http://localhost:8080/goods';
+  const url = header + 'goods';
   getRequest(url, data, callback);
 };
 
 export const getGoodsByGoodsType = (data, callback) => {
-  const url = 'http://localhost:8080/goods/getGoodsByGoodsType';
+  const url = header + 'goods/getGoodsByGoodsType';
   getRequest(url, data, callback);
 };
 
 export const getGoodsByName = (data, callback) => {
-  const url = 'http://localhost:8080/goods/search';
+  const url = header + 'goods/search';
   getRequest(url, data, callback);
 };
 
 export const getPopularGoods = (data, callback) => {
-  const url = 'http://localhost:8080/goods/getPopularGoods';
+  const url = header + 'goods/getPopularGoods';
   getRequest(url, data, callback);
 };
 
 export const getAllAuctions = (data, callback) => {
-  const url = 'http://localhost:8080/goods/getAllAuctions';
+  const url = header + 'goods/getAllAuctions';
   getRequest(url, data, callback);
 };
 
 export const getAuctionByAuctionId = (data, callback) => {
-  const url = 'http://localhost:8080/goods/getAuctionByAuctionId';
+  const url = header + 'goods/getAuctionByAuctionId';
   getRequest(url, data, callback);
 };
 
 export const updateAuction = (data, callback) => {
-  const url = 'http://localhost:8080/goods/updateAuction';
+  const url = header + 'goods/updateAuction';
   postRequest(url, data, callback);
 };
 
 export const deleteGoodsByGoodsId = (goodsId, callback) => {
-  const url = 'http://localhost:8080/goods/delete/' + goodsId;
+  const url = header + 'goods/delete/' + goodsId;
   const data = {};
   deleteRequest(url, data, callback);
 };
 
 export const addAuction = (data, callback) => {
-  const url = 'http://localhost:8080/goods/addAuction';
+  const url = header + 'goods/addAuction';
   putRequest(url, data, callback);
 };
 
 export const editGoods = (data, callback) => {
-  const url = 'http://localhost:8080/goods/editGoods';
+  const url = header + 'goods/editGoods';
   postRequest(url, data, callback);
 };
 
 export const deleteAuctionByAuctionId = (data, callback) => {
-  const url = 'http://localhost:8080/goods/deleteAuctionByAuctionId';
+  const url = header + 'goods/deleteAuctionByAuctionId';
   deleteRequest(url, data, callback);
 };
 
 export const editAuction = (data, callback) => {
-  const url = 'http://localhost:8080/goods/editAuction';
+  const url = header + 'goods/editAuction';
   postRequest(url, data, callback);
 };
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,52 +1,54 @@
-import { postRequest, getRequest,putRequest} from '../utils/ajax';
+import { postRequest, getRequest, putRequest} from '../utils/ajax';
+
+const header = 'http://ec2-100-25-4-241.compute-1.amazonaws.com:8080/';
 
 export const login = (data, callback) => {
-  const url = 'http://localhost:8080/sso/login';
+  const url = header + 'sso/login';
   postRequest(url, data, callback);
 };
 
 export const register = (data, callback) => {
-  const url = 'http://localhost:8080/user/register';
+  const url = header + 'user/register';
   postRequest(url, data, callback);
 };
 
 export const logout = (callback) => {
-  const url = 'http://localhost:8080/sso/logout';
+  const url = header + 'sso/logout';
   postRequest(url, {}, callback);
 };
 
 export const checkSession = (callback) => {
-  const url = 'http://localhost:8080/sso/checkSession';
+  const url = header + 'sso/checkSession';
   getRequest(url, {}, callback);
 };
 
 export const getOrdersByUserId = (data, callback) => {
-  const url = 'http://localhost:8080/order/getOrdersByUserId';
+  const url = header + 'order/getOrdersByUserId';
   getRequest(url, data, callback);
 };
 
 export const addOrder = (data, callback) => {
-  const url = 'http://localhost:8080/order/addOrder';
+  const url = header + 'order/addOrder';
   putRequest(url, data, callback);
 };
 
 export const getAllUsers = (data, callback) => {
-  const url = 'http://localhost:8080/user/getAllUsers';
+  const url = header + 'user/getAllUsers';
   getRequest(url, data, callback);
 };
 
 export const getAllOrders = (data, callback) => {
-  const url = 'http://localhost:8080/order/getAllOrders';
+  const url = header + 'order/getAllOrders';
   getRequest(url, data, callback);
 };
 
 export const changeUserStatusByUserId = (data, callback) => {
-  const url = 'http://localhost:8080/user/changeUserStatusByUserId';
+  const url = header + 'user/changeUserStatusByUserId';
   postRequest(url, data, callback);
 };
 
 export const getRecommendGoods = (number, callback) => {
-  const url = 'http://localhost:8080/goods/getRecommendGoods/' + number;
+  const url = header + 'goods/getRecommendGoods/' + number;
   const data = {};
   getRequest(url, data, callback);
 };
@@ -56,12 +58,12 @@ export const getRecommendGoods = (number, callback) => {
 //     postRequest(url, data, callback);
 // };
 //
-export const editUser = (data,callback) => {
+export const editUser = (data, callback) => {
     const url = `http://localhost:8080/user/editUser`;
     postRequest(url, data, callback);
 };
 
-export const getUserById = (userId,callback) => {
+export const getUserById = (userId, callback) => {
   const url = `http://localhost:8080/user/getUserById/` + userId;
   const data = {};
   getRequest(url, data, callback);
