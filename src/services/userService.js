f21656522961b3951923be91b5301c15327d47e1
ALAN <48608756+HaoningLan@users.noreply.github.com>
2020-09-08 18:01:54 +0800

    Update userService.js

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,6 +1,6 @@
 import { postRequest, getRequest, putRequest} from '../utils/ajax';
 
-const header = 'http://ec2-100-25-4-241.compute-1.amazonaws.com:8080/';
+const header = 'https://ec2-100-25-4-241.compute-1.amazonaws.com:8080/';
 
 export const login = (data, callback) => {
   const url = header + 'sso/login';
